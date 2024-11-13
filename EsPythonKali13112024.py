2548062fcaa1637d67a5b8997a1da7b3ea823fa0
LiviuMirzac <liviumirzac77@gmail.com>
2024-11-13 10:30:48 -0500

    EsPython13/11/2024

---
+++ EsPythonKali13112024.py
@@ -0,0 +1,4 @@
+città = input("Inserisci il nome della tua città: ")
+animale = input("Inserisci il nome del tuo animale: ")
+nome_band = città + " " + animale
+print(f"Il nome della tua band potrebbe essere: {nome_band}")
